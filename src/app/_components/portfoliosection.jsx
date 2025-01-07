6638936280b6860e8632b9c4e9cbe0d49ff50c76
CoolGuyGames <theforestgriefer@gmail.com>
2025-01-07 14:06:23 -0500

    fixed key prop

--- src/app/_components/portfoliosection.jsx
+++ src/app/_components/portfoliosection.jsx
@@ -15,9 +15,8 @@ export default async function PortfolioSection() {
       <SearchBar />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-[80vw] 2xl:w-[70vw] mt-8 place-items-center">
         {projects.map((project, id) => (
-          <div>
+          <div key={id}>
             <PortfolioResult
-              key={id}
               link={project.urlTitle}
               title={project.title}
               type={project.type}
