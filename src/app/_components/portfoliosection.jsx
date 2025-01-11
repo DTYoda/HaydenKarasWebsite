d137351c11df68e25ca7e2cf634e6af2527747ec
CoolGuyGames <theforestgriefer@gmail.com>
2025-01-10 23:21:57 -0500

    fixed images on project pages

--- src/app/_components/portfoliosection.jsx
+++ src/app/_components/portfoliosection.jsx
@@ -5,14 +5,12 @@ import { PrismaClient } from "@prisma/client";
 export default async function PortfolioSection() {
   const prisma = new PrismaClient();
   const projects = await prisma.projects.findMany();
-  console.log(projects.images);
 
   return (
     <div className="min-h-screen flex flex-col items-center">
       <h1 className="sm:text-[4vw] text-[10vw] p-8 text-center text-7xl uppercase">
-        Portfolio - Work in Progress
+        Portfolio
       </h1>
-      <SearchBar />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-[80vw] 2xl:w-[70vw] mt-8 place-items-center">
         {projects.map((project, id) => (
           <div key={id}>
